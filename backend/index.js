99cee108c0a87d3481fa7a5a42d6b4f560469dfa
Suhani <suhaninegi124@gmail.com>
2026-03-28 17:14:34 +0530

    Comments Added

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ import GroupRouter from "./routes/group.js";
 import ExpenseRouter from "./routes/expense.js";
 import 'dotenv/config';
 
-
 import {restrictToLoggedInUserOnly} from "./middleware/auth.js"
 
-//iske bina req ni jari thi front end se backend ko 
+//To ensure cross origin connection (as frontend and backend both are on diff routes...) 
 import cors from "cors";
+
+//Enable reading of Cookie
 import cookieParser from "cookie-parser";
 
 
@@ -19,32 +20,33 @@ app.use(cors({
     origin:"http://localhost:5173", //will allow req only from this 
     credentials:true //allows cookie and authentication headers to be sent
 }));
-app.use(cookieParser()); //bcz we are using cookie
+app.use(cookieParser()); //Because we are using cookie
 
 
-//this is a builtin middleware to parse form data into readable javascript object
+//Built in middleware to parse form data into readable javascript object
 app.use(express.urlencoded({extended:false}));
 
-
-//this is a builtin middleware used to parse json data into javascript object
+//Built In middleware to parse json data into javascript object
 app.use(express.json());
 
-//connect MONGODB
+// connect MONGODB
 // const URL="mongodb://127.0.0.1:27017/split-mates";
-const URL=process.env.MONGO_URI;
+
+//Database connection using environment variable
+const URL=process.env.MONGO_URI; //Database connection using environment variable
 mongoose.connect(URL).then(()=>console.log("MongoDB connected")).catch((err)=>console.log("Mongo Error",err));
 
-//making routes go to user routes
+// Routes to handles user Signup and Login
 app.use('/api/user',UserRouter);
 
-//making routes for the access of data in the dashboard
+//Routes for the access of data in the dashboard
 app.use('/api/private',restrictToLoggedInUserOnly,PrivateRouter);
 
-//making route to create group
-// middleware will automatifally add the user email and _id
+// Route to create group
+// middleware will automatifally add the current user email and _id
 app.use('/api/group',restrictToLoggedInUserOnly,GroupRouter);
 
-//making route to add expenses
+// Routes to add expenses
 app.use('/api/expense/',restrictToLoggedInUserOnly,ExpenseRouter)
 
 const PORT=7000;
