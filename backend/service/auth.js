382ab4d6dfbcda03d7a17d5c26bef2dcf7163f83
Suhani <suhaninegi124@gmail.com>
2026-03-28 17:15:59 +0530

    Comments Added

--- backend/service/auth.js
+++ backend/service/auth.js
@@ -1,28 +1,41 @@
-//to generate token and send token in cookie kyunki browser ke pass cookie hota hai
-const secretKey="Suhani$123@$";
+// To generate token and send token in cookie
+// Because browser have cookies
+const secretKey = "Suhani$123@$";
 
 import jwt from "jsonwebtoken";
 
-
-//will utilize this in middleware to generate token
-function setUser(user){
-    return jwt.sign({
-        _id:user._id,
-        email:user.email,
-    },secretKey);
+// Will utilize this to GENERATE TOKEN when user Logs in ...
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secretKey,
+  );
 }
 
-//to verify token and return the user details ie email and _id
-function getUser(token)
-{
-    if(!token) return null;
-    try{
-        //if it get verified than it will return the decoded payload
-        //if not get verified it will cause error and hence will go to catch block
-        return jwt.verify(token,secretKey);
-    }catch(err){
-        return null;
-    }
+// VERIFY TOKEN and RETURN USER DETAILS (Used for Authentication)
+function getUser(token) {
+
+  if (!token) return null;
+
+  try {
+
+    // Decode payload  // Will go to catch block if invalid token
+    const result = jwt.verify(token, secretKey);
+    return result;
+
+  } catch (err) {
+    return null;
+  }
 }
 
-export {setUser, getUser};
+export { setUser, getUser };
+
+
+// {
+//   _id: '69b530ab63641281ac8ea61a',
+//   email: 'suhani@gmail.com',
+//   iat: 1774689818
+// }
