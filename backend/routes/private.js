43e1f7b6135215b0147ecaff7a7e8926d2d758a5
Suhani <suhaninegi124@gmail.com>
2026-03-28 17:15:07 +0530

    Comments Added

--- backend/routes/private.js
+++ backend/routes/private.js
@@ -3,8 +3,8 @@ import {findUser} from "../controllers/private.js";
 
 const router=Router();
 
-//find the user
-//eg api/private/find-user?username=john
+// SEARCH USER to add in the group during CREATE GROUP
+// eg api/private/find-user?username=john
 
 router.get("/find-user",findUser);
 
