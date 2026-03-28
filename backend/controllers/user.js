04cca3ac38ee5787b699933c20237896ea8f26c2
Suhani <suhaninegi124@gmail.com>
2026-03-28 19:13:07 +0530

    Formatting done and Comments Updation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,24 +1,25 @@
 import User from "../models/user.js";
 import { getUser, setUser } from "../service/auth.js";
 
+// CREATE USER ACCOUNT
 async function handleAddNewUser(req, res) {
   const { name, userName, email, password } = req.body;
   const lowerEmail = email.toLowerCase();
 
   try {
-    //check if username already exist
+    // CHECK IF USERNAME ALREADY EXIST
     const existingUsername = await User.findOne({ userName });
     if (existingUsername) {
       return res.status(400).json({ message: "Username already taken" });
     }
 
-    //check if email already exist
+    // CHECK IF EMAIL ALREADY EXIST
     const existingEmail = await User.findOne({ email: lowerEmail });
     if (existingEmail) {
       return res.status(400).json({ message: "Email already exists" });
     }
 
-    //now we can create entry peacefully
+    // CREATE NEW USER IN DB
     await User.create({
       name: name,
       userName: userName,
@@ -27,43 +28,50 @@ async function handleAddNewUser(req, res) {
     });
 
     return res.status(201).json({ message: "User Account Created" });
+
   } catch (error) {
+
     res.status(500).json({ message: "Server error", error });
+
   }
 }
 
-//api/user/   get
+
+// LOG IN 
 async function handleLoginUser(req, res) {
   try {
     const { email, password } = req.body;
     const lowerEmail = email.toLowerCase();
-    const user = await User.findOne({ email: lowerEmail });
-    // console.log(user);
 
-    //user exist or not
+    // FIND USER EMAIL
+    const user = await User.findOne({ email: lowerEmail });
+    
+
+    // USER EMAIL EXIST OR NOT
     if (!user) {
       return res.status(400).json({
         message: "User not found",
       });
     }
 
-    //compare the password
+    // COMPARE PASSWORD
     const isMatch = user.password === password;
 
     if (!isMatch) {
       return res.status(400).json({
-        message: "Invalid credentials",
+        message: "Incorrect Password",
       });
     }
 
-    //everything fine generate JWT
+    // GENERATE JWT TOKEN for future authentication
     const token = setUser(user);
     // console.log(token);
 
+
+    // SAVE TOKEN IN THE COOKIE
     res.cookie("token", token, {
-      httpOnly: true, //protect coookie from front end/client side javascript code
-      secure: false, //production me true krna.. ture krne se it will ensure that cookie will be sended to only https
-      sameSite: "lax",
+      httpOnly: true, // Front end will not be able to modify and access cookie (ie JWTtoken) (but can see in network tab) only http req can access it
+      secure: false, // production me true krna.. true will ensure that cookie will be sended to only https websites
     });
 
     // console.log(user);
@@ -74,14 +82,16 @@ async function handleLoginUser(req, res) {
       email:user.email,
       _id:user._id,
     });
+
+
   } catch (err) {
     return res.status(500).json({
-      message: "server error",
+      message: "Server Error",
     });
   }
 }
 
-//to check if user is logged in or not
+// USER ALREADY LOGGED IN OR NOT ???
 async function checkAuth(req, res) {
   const token = req.cookies.token;
 
@@ -90,24 +100,21 @@ async function checkAuth(req, res) {
   }
 
   const user = getUser(token); // JWT verify function
-  
-  
 
   const currUser = await User.findOne({email:user.email});
-  console.log("i am user from the auth")
-  console.log(currUser);
+
 
   if (!user) return res.status(401).json({ isLoggedIn: false });
 
   return res.status(200).json({ isLoggedIn: true, userName:currUser.userName , email : currUser.email ,_id:currUser._id });
 }
 
+
+// LOGOUT - clear token from cookie
 function logOutUser(req, res) {
-  res.cookie("token", "", {
+  res.clearCookie("token", {
     httpOnly: true,
     secure: false, // production me true
-    expires: new Date(0), // expire immediately
-    sameSite: "lax",
   });
 
   return res.status(200).json({ message: "Logged out sucessfully" });
