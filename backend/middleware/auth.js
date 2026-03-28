25f07d6f30b33914f679a65f482c8bba3d1d583e
Suhani <suhaninegi124@gmail.com>
2026-03-28 17:13:59 +0530

    Comments Added

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,12 +7,13 @@ async function restrictToLoggedInUserOnly(req,res,next){
         return res.status(401).json({message:"You need to login first"})
     }
 
-    const user=getUser(token); //service me hai to verify the token
+    const user=getUser(token); // Verify the request JWT token 
+    
     if(!user){
         return res.status(401).json({message:"Invalid token !!"});
     }
 
-    //put this user email and id in the request object
+    //put this user email and _id in the request object
     req.user=user;
     next(); //lets move forward
 }
